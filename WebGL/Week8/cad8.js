3eaf39693c7f9bc5bb193029561529a6e7dacdec
Nancy Wang <n0w1253@gmail.com>
2015-08-22 12:38:47 -0600

    make the line disappear

--- WebGL/Week8/cad8.js
+++ WebGL/Week8/cad8.js
@@ -176,8 +176,8 @@ function createTexture(img){
 	var t = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, t);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR); 
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST); 
     gl.generateMipmap(gl.TEXTURE_2D);
 	return t;
 }
